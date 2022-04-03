4875594c246be8b777c85dc4b49892c85afb4d56
CharlesK <36734075+CharlesKluznik@users.noreply.github.com>
2022-04-03 12:53:44 -0400

    Updated from Rpi

--- final/AHRS/comm.py
+++ final/AHRS/comm.py
@@ -8,38 +8,62 @@ import board
 from digitalio import DigitalInOut
 import adafruit_rfm9x
 
+rfm9x = None
+spi = None
+CS = None
+RESET = None
+FREQ = None
+
+sleep(2)
 try:
-    rfm9x = None
     FREQ = 915.0
     CS = DigitalInOut(board.CE1)
     RESET = DigitalInOut(board.D25)
     spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, FREQ)
+    rfm9x.tx_power = 23
 except Exception as ex:
-    print(f'Exception: {ex}')
-    pass
+        print(f'lora setup exception: {ex}')
+        pass
+
 
 def lora_init():
-    # Configure LoRa Radio
     try:
+        FREQ = 915.0
+        CS = DigitalInOut(board.CE1)
+        RESET = DigitalInOut(board.D25)
+        spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
         rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, FREQ)
         rfm9x.tx_power = 23
     except Exception as ex:
-        print(f'Exception: {ex}')
+        print(f'lora_init exception: {ex}')
         pass
 
 
 def lora_transmit(msg:str):
     try:
+        FREQ = 915.0
+        CS = DigitalInOut(board.CE1)
+        RESET = DigitalInOut(board.D25)
+        spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, FREQ)
+        rfm9x.tx_power = 23
         data: bytes = bytes(f'{msg}', encoding='utf8')
         rfm9x.send(data)
     except Exception as ex:
-        print(f'Exception: {ex}')
+        print(f'lora_transmit exception: {ex}')
         pass
 
 def lora_hearbeat():
     try:
-        data: bytes = bytes(f'RPi Transmission on {FREQ} at {get_timestamp}', encoding='utf8')
+        FREQ = 915.0
+        CS = DigitalInOut(board.CE1)
+        RESET = DigitalInOut(board.D25)
+        spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, FREQ)
+        rfm9x.tx_power = 23
+        data: bytes = bytes(f'RPi Transmission on {FREQ} at {get_timestamp()}', encoding='utf8')
         rfm9x.send(data)
     except Exception as ex:
-        print(f'Exception: {ex}')
+        print(f'lora_heartbeat exception: {ex}')
         pass

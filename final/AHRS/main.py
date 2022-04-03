cb173d62911df38dd603e5e336fe66d69c4b740c
CharlesK <36734075+CharlesKluznik@users.noreply.github.com>
2022-04-03 12:53:09 -0400

    Updated from RPi

--- final/AHRS/main.py
+++ final/AHRS/main.py
@@ -7,54 +7,70 @@ import sys
 import os
 from time import sleep
 
-print('Payload init.\n')
-print(f'Time: {get_timestamp()\n')
-sys.stdout = open(os.path.join(sys.path[0], 'payload_log.txt'), 'a')
+print('Payload init.\n', flush=True)
+print(f'Time: {get_timestamp()}\n')
+os.system('ls /dev/tty* >> ports.txt')
+os.system('sudo date >> ports.txt')
+sys.stdout = open(os.path.join(sys.path[0], 'payload_log.txt'), 'w')
 sys.stderr = sys.stdout
 
 SERIAL_PORT: str = '/dev/ttyACM0'
 
 if (__name__ == "__main__"):
-    print(f'\n{get_timestamp()} RPi Online.\n')
+    print(f'\n{get_timestamp()} RPi Online.\n', flush=True)
     #initialize lora radio and send a hearbeat
-    lora_init()
-    lora_hearbeat()
+    try:
+        sleep(1)
+        lora_init()
+        sleep(1)
+        lora_hearbeat()
+    except Exception as ex:
+        print(f'lorainit exception: {ex}')
 
-    #wait for serial port
-    sleep(5)
     #log teensy serial data to file
-    print(f'{get_timestamp()} Begin Serial Logging.')
-    log_teensy(SERIAL_PORT)
-    print(f'{get_timestamp()} End Serial Logging.')
+    print(f'{get_timestamp()} Begin Serial Logging.', flush=True)
+    try: log_teensy(SERIAL_PORT)
+    except Exception as ex:
+        print(f'log_teensy exception: {ex}', flush=True)
+    print(f'{get_timestamp()} End Serial Logging.', flush=True)
     lora_hearbeat()
 
     #clean serial data
-    print(f'{get_timestamp()} Begin Cleaning Data.')
-    clean_data(os.path.join(sys.path[0], 'serial.txt'))
-    print(f'{get_timestamp()} End Cleaning Data.')
+    print(f'{get_timestamp()} Begin Cleaning Data.', flush=True)
+    try: clean_data(os.path.join(sys.path[0], 'serial.txt'))
+    except Exception as ex:
+        print(f'clean_data exception: {ex}')
+    print(f'{get_timestamp()} End Cleaning Data.', flush=True)
     lora_hearbeat()
 
     #process flight data and generate output quaternions
-    print(f'{get_timestamp()} Begin Orientation Estimate.')
-    process_orientation(input=os.path.join(sys.path[0], 'cleaned_data.txt'), verbose=False)       
-    print(f'{get_timestamp()} End Orientation Estimate.')
+    print(f'{get_timestamp()} Begin Orientation Estimate.', flush=True)
+    try: process_orientation(input=os.path.join(sys.path[0], 'cleaned_data.txt'), verbose=False)
+    except Exception as ex:
+        print(f'processs_orientation exception: {ex}')
+    print(f'{get_timestamp()} End Orientation Estimate.', flush=True)
     lora_hearbeat()
 
     #generate position estimate
-    print(f'{get_timestamp()} Begin Position Estimate.') 
-    coordinate: Tuple[str, str, float, float] = process_position()
-    print(f'{get_timestamp()} End Position Estimate.')
+    print(f'{get_timestamp()} Begin Position Estimate.', flush=True) 
+    try: coordinate: Tuple[str, str, float, float] = process_position()
+    except Exception as ex:
+        print(f'process_position exception: {ex}')
+    print(f'{get_timestamp()} End Position Estimate.', flush=True)
     lora_hearbeat()
 
     #transmit orientation via radio
-    print(f'{get_timestamp()} RPi Finished. Transmitting coordinate.')
+    print(f'{get_timestamp()} RPi Finished. Transmitting coordinate.', flush=True)
 
     iterations: int = 0
     while True:
-        i +=1
-        if (i == 5):
-            copy_logs()
+        iterations +=1
+        if (iterations == 5):
+            try: copy_logs()
+            except Exception as ex:
+                print(f'copy_logs exception: {ex}')
         else:
             #transmit position estimate
             lora_transmit(f'{coordinate[2]} {coordinate[3]} {coordinate[0]}{coordinate[1]}')
+            print(f'Transmitted {get_timestamp()}', flush=True)
             sleep(0.5)
